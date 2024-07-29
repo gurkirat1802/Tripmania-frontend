cdab582093c3a8f9f0cb38e60ee90d70d083837c
gurkirat1802 <85217914+gurkirat1802@users.noreply.github.com>
2024-07-29 21:10:10 +0530

    Update Signup.jsx

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -23,7 +23,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:3000/api/v1/signup', {
+      const response = await fetch('https://tripmania-backend.onrender.com/api/v1/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
