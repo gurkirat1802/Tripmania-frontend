b364faaf421dec7c84887ca6c409ce0a067d9894
gurkirat1802 <85217914+gurkirat1802@users.noreply.github.com>
2024-07-29 21:09:30 +0530

    Update Login.jsx

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -21,7 +21,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:3000/api/v1/login', formData);
+      const response = await axios.post('https://tripmania-backend.onrender.com/api/v1/login', formData);
       console.log(response.data); 
       navigate("/user/dashboard");
       alert("Login successful");
